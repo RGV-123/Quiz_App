3dfedcab4c2670774d44b276d0597778d2872553
RGV-123 <23eg104a61@anurag.edu.in>
2026-03-30 09:53:19 +0530

    Fixed GenerationType SEQUENCE to IDENTITY for MySQL

--- src/main/java/com/quiz_app/model/Quiz.java
+++ src/main/java/com/quiz_app/model/Quiz.java
@@ -10,36 +10,11 @@ import java.util.List;
 public class Quiz {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
 
     @ManyToMany
     private List<Question> questions;
-
-    // Getters and Setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public List<Question> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(List<Question> questions) {
-        this.questions = questions;
-    }
 
 }
